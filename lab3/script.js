46c1ef0271c6a35cb2b48a4f41856ae3491b9608
VladKorniichuk <162552114+VladKorniichuk@users.noreply.github.com>
2024-04-02 18:55:29 +0200

    Add files via upload

--- lab3/script.js
+++ lab3/script.js
@@ -4,6 +4,7 @@ let c = 23.2
 
 // alert(a+b+c)
 
+alert("Hello world")
 
 document.getElementById("wynikD").innerHTML = a+b+c;
 document.getElementById("wynikO").innerHTML = a-b-c;
@@ -17,12 +18,30 @@ for(let i = 0; i <= 100; i ++){
 }
 
 
-//function myFunction() {
-    //document.getElementById("fm1").innerHTML = 
-    //let p = (a+b+c)/2;
-   // let n = Math.sqrt(p(p-a)*(p-b)*(p-a))
-    
-//}
-let some = prompt("Enter something: ")
+function obliczPoleTrojkata(d, e, f) {
+    var p = (d + e + f) / 2;
+    var pole = Math.sqrt(p * (p - d) * (p - e) * (p - f));
+    return pole;
+}
+
+var d = 3;
+var e = 4;
+var f = 5;
+var pole = obliczPoleTrojkata(d, e, f);
+console.log("Pole trójkąta wynosi: " + pole);
+
+let some = prompt("Enter ur name: ")
 //console.log(some)
-alert("hi" + some);
+alert("hi " + some);
+
+
+var liczba1 = prompt("Wprowadź pierwszą liczbę całkowitą:");
+var liczba2 = prompt("Wprowadź drugą liczbę całkowitą:");
+
+liczba1 = parseInt(liczba1);
+liczba2 = parseInt(liczba2);
+
+var suma = liczba1 + liczba2;
+
+document.write("<h2>Wynik dodawania:</h2>");
+document.write("<p>" + liczba1 + " + " + liczba2 + " = " + suma + "</p>");
